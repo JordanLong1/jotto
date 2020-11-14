05e06601e3112bf0ae76915bc40bf86397676fb0
Jordan <jordanleelong5@gmail.com>
2020-11-14 10:41:38 -0800

    added function to getLetterMatchCount

---
+++ src/helpers/index.js
@@ -0,0 +1,13 @@
+/**
+ * @method - get letter match count
+ * @param {string} guessedWord -guessed word
+ * @param {string} secretWord -secret word
+ * @returns {number} - number of letters matched between guessed word and secret
+ */
+
+
+export function getLetterMatchCount(guessedWord, secretWord) {
+    const secretLetterSet = new Set(secretWord.split('')); 
+    const guessedLetterSet = new Set(guessedWord.split('')); 
+    return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter)).length;
+};
