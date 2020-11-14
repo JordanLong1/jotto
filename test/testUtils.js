4384dbd18afcebe46935ea9c65ddb99ec163bf8f
Jordan <jordanleelong5@gmail.com>
2020-11-13 18:08:51 -0800

    added checkprops function

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,4 @@
-
+import checkPropTypes from 'check-prop-types'
 
 
 /**
@@ -12,3 +12,14 @@
 // }
 
 export const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`)
+
+
+export const checkProps = (component, conformingProps) => {
+    const propError = checkPropTypes(
+        component.propTypes, 
+        conformingProps, 
+        'prop',
+        component.name
+        )
+        expect(propError).toBeUndefined();
+}
