d193007a7f749fc87be7fc067ff94dea55271ccd
Jordan <jordanleelong5@gmail.com>
2020-11-16 10:47:30 -0800

    wrote tests for when success piece of state is false

--- src/Input.test.js
+++ src/Input.test.js
@@ -7,6 +7,8 @@ import Input from './Input';
 
 const setUp = (initialState={}) => {
     const store = storeFactory(initialState); 
+    //added store as prop to connect component within the tests, used shallow to createa  virtual dom, then used .dive to get
+    //child component (input)
     const wrapper = shallow(<Input store={store} />).dive().dive(); 
     return wrapper;
 };
@@ -14,16 +16,24 @@ const setUp = (initialState={}) => {
 
 describe('render', () => {
     describe('word has not been guessed', () => {
+        let wrapper; 
+        beforeEach(() => {
+            const initialState = {success: false} //we can use this initalState for all of our tests
+            wrapper = setUp(initialState); 
+        })
         test('renders component without error', () => {
-
+            const component = findByTestAttr(wrapper, 'component-input'); 
+            expect(component.length).toBe(1);
         }); 
 
         test('renders input box', () => {
-
+            const inputBox = findByTestAttr(wrapper, 'input-box'); 
+            expect(inputBox.length).toBe(1);
         }); 
 
         test('renders submit button', () => {
-
+            const submitButton = findByTestAttr(wrapper, 'submit-button'); 
+            expect(submitButton.length).toBe(1);
         });
     });
     describe('word has been guessed', () => {
