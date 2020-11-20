ac8ed5313eb5ef736f1dc69d884b0f10c323c394
Jordan <jordanleelong5@gmail.com>
2020-11-19 21:35:35 -0800

    wrote 4 tests for App component  checking for state and props

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,43 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react'
 import App from './App';
+import {shallow} from 'enzyme'; 
+import {storeFactory} from '../test/testUtils'
 
-test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+
+const setUp = (state={}) => {
+  const store = storeFactory(state); 
+ console.log(store)
+  const wrapper = shallow(<App store={store} />).dive().dive(); 
+  return wrapper;
+};
+
+test('App component has success as a prop', () => {
+  const success = true; 
+  const wrapper = setUp({success}); 
+  const successProp = wrapper.instance().props.success; 
+  console.log(wrapper.instance().props)
+  expect(successProp).toBe(success);
+
 });
+
+test('has access to secretWord state', () => {
+  const word = 'party'; 
+  const wrapper = setUp({word}); 
+  const secretWordProp = wrapper.instance().props.word; 
+  console.log(wrapper.instance().props)
+  expect(secretWordProp).toBe(word);
+});
+
+test('has access to guessedWords state', () => {
+  const guessedWords = [{guessedWord: 'train', letterMatchCount: 3}]; 
+  const wrapper = setUp({guessedWords}); 
+  const guessedWordState = wrapper.instance().props.guessedWords; 
+  console.log(wrapper.instance().props)
+  expect(guessedWordState).toBe(guessedWords);
+}); 
+
+test('getSecretWord action creator is a function on the props', () => {
+  const wrapper = setUp(); 
+  const secretWordProp = wrapper.instance().props.getSecretWord; 
+  expect(secretWordProp).toBeInstanceOf(Function);
+}); 
