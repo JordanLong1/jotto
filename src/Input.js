4553abfc98a91a9b0d99eafa4f2b296f4786b45f
Jordan <jordanleelong5@gmail.com>
2020-11-19 19:41:00 -0800

    imported guessWord action creator to pass test

--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import {connect} from 'react-redux'; 
-
+import {guessWord} from './actions/index';
 class Input extends React.Component {
     render() {
         const contents = this.props.success 
@@ -32,4 +32,4 @@ const mapStateToProps = ({success})=> {
     return {success}
 }
 
-export default connect(mapStateToProps)(Input)
+export default connect(mapStateToProps, {guessWord})(Input)
