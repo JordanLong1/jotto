0cc2d7080c944c7e61d12a013938369bfcc8f64e
Jordan <jordanleelong5@gmail.com>
2020-11-19 19:18:42 -0800

    finished secretWordReducer to pass test

--- src/reducers/secretWordReducer.js
+++ src/reducers/secretWordReducer.js
@@ -1,3 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+import {actionTypes} from '../actions/index'
+
+
 export default (state=null, action) => {
-    return state
+
+    switch(action.type) {
+        case actionTypes.SET_SECRET_WORD:
+            return action.payload; 
+            default:
+                return state
+    }
 }
